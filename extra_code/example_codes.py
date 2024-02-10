97cf21c47d93bb6167508bf67017258f1e6667e9
bwug <119032334+bwug@users.noreply.github.com>
2024-02-10 20:27:47 +0000

    Added lecture 2's notes

---
+++ extra_code/example_codes.py
@@ -0,0 +1,38 @@
+# Arithmetic
+
+a = 6 + 7
+
+# Assignment
+
+a = 6
+
+# Comparison
+
+a = 6 < 7
+
+# Logical
+
+a = True and False
+
+# Identity
+
+a = 6
+b = a
+b is a # True
+
+# Membership
+
+a = "hello world!"
+b = "ello" in a # True
+
+# Bitwise
+
+a = 6
+b = 7
+
+c = a & b # Notice how we have two different ways to compare using "and" statements
+
+# Casting
+
+a = "6"
+b = int(a) # -> 6
